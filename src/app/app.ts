41c255b99a29897b8170526536245e8650a145fa
Pedro Maranhão <pedrinhoelloco@outlook.com.br>
2025-09-01 22:13:04 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './components/shared/menu/menu';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Menu
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('projetoWeb');
+  // Criar um objeto do tipo HttClient
+  private http = inject(HttpClient);
+
+  // função para ao abrir a página consultar as categorias na API
+  ngOnInit() {
+    // fazendo uma chamada para o serviço de consulta de categorias
+    this.http.get('http://localhost:8081/api/v1/categorias')
+            .subscribe((dados) => console.table(dados));
+  }
 }
